083ece86b2ced656249aca131d2bd29158acb8c9
nilekurt <nilekurt@gmail.com>
2016-11-12 12:56:04 +0800

    Add initial drawing code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 var c = document.getElementById("screen");
 var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(640,480);
-ctx.stroke();
+
+var posX = 0;
+
+var mainLoop = function()
+{
+	posX += 1;
+	if ( posX > c.width )
+	{
+		posX = 0;
+	}
+
+	ctx.moveTo(0,0);
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.lineTo(posX, c.height);
+	ctx.stroke();
+
+	animFrame( mainLoop, c );
+};
+
+animFrame( mainloop, c);
