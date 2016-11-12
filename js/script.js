796791d2b62fe2922572c102c6237414028eb31c
nilekurt <nilekurt@gmail.com>
2016-11-12 13:31:45 +0800

    Decoupled logic from frame timing

--- js/script.js
+++ js/script.js
@@ -8,15 +8,17 @@ var animFrame = window.requestAnimationFrame ||
 var c = document.getElementById("screen");
 var ctx = c.getContext("2d");
 
-var posX = 0.0;
+var angle = 0.0;
+var currentTime = Date.now();
 
 var tick = function()
 {
-	posX += 0.01;
-	if ( posX > 2.0 )
-	{
-		posX = 0.0;
-	}
+	var newTime = Date.now();
+	var delta = newTime - currentTime;
+	currentTime = newTime;
+	
+	angle += 0.01 * delta;
+	angle %= 2.0;
 }
 
 var draw = function()
@@ -29,7 +31,7 @@ var draw = function()
 	ctx.save();
 
 	ctx.translate(c.width/2, c.height/2);
-	ctx.rotate(Math.PI * posX);
+	ctx.rotate(Math.PI * angle);
 	ctx.fillText("我爱小宝！", 0, 0);
 
 	ctx.restore();
