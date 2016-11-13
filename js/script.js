77d22ecd0d844e5914e66e2235c47efdb517eeca
nilekurt <nilekurt@gmail.com>
2016-11-13 17:07:32 +0800

    Added ajax result callback

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-$.getScript("/js/GameEngine.js", function() {});
+$.getScript("/js/GameEngine.js",
+function( data, textStatus, jqxhr ) {
+  console.log( data ); // Data returned
+  console.log( textStatus ); // Success
+  console.log( jqxhr.status ); // 200
+  console.log( "Load was performed."); 
+}
+);
 
 var JSTest = JSTest || {};
 
