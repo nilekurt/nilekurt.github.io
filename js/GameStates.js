529996287e94a4d0f989dd78735d7cef933be699
nilekurt <nilekurt@gmail.com>
2016-11-14 14:53:32 +0800

    Wrong

--- js/GameStates.js
+++ js/GameStates.js
@@ -19,8 +19,9 @@ JSTest.World.prototype.interpolate = function(prevWorld, alpha)
 {
     var interpWorld = new JSTest.World();
 
+    var tWorld = prevWorld instanceof JSTest.World;
     var a = this._angle;
-    var b instanceof JSTest.World = prevWorld._angle;
+    var b = tWorld._angle;
 
     if (abs(b - a) <= Math.PI)
     {
