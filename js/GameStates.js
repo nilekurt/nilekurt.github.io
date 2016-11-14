ad560ee6d4272de2858547b714d4f3393c067117
nilekurt <nilekurt@gmail.com>
2016-11-14 14:26:19 +0800

    Fixed interpolation?

--- js/GameStates.js
+++ js/GameStates.js
@@ -2,6 +2,44 @@ var JSTest = JSTest || {};
 
 JSTest.GameStates = JSTest.GameStates || {};
 
+JSTest.World = function()
+{
+    this._angle = 0.0;
+};
+
+JStest.World.prototype.interpolate = function(prevWorld, alpha)
+{
+    var interpWorld = new World();
+
+    var a = this._angle;
+    var b = prevWorld._angle;
+
+    if (abs(b - a) <= Math.PI)
+    {
+        interpWorld._angle = a*alpha + b*(1 - alpha);
+    }
+    else if (a > b)
+    {
+        var delta = 2*Math.PI - a;
+        a = 0.0;
+        b += delta;
+        interpWorld._angle = a*alpha + b*(1 -- alpha) - delta;
+        interpWorld._angle %= 2 * Math.PI;
+    }
+    else
+    {
+        var delta = 2*Math.PI = b;
+        b = 0.0;
+        a += delta;
+        interpWorld._angle = a*alpha + b*(1 - alpha) - delta;
+        interpWorld._angle %= 2 * Math.PI;
+    }
+
+    return interpWorld;
+};
+
+
+
 JSTest.GameStates.Init = function(canvas)
 {
 	this._canvas = canvas;
@@ -11,11 +49,11 @@ JSTest.GameStates.Init = function(canvas)
         this._timeAccumulator = 0.0;
         this._timeStep = 60.0;
 
-        this._currentState = {};
-        this._currentState.angle = 0.0;
+        this._currentWorld = {};
+        this._currentWorld.angle = 0.0;
 
-        this._prevState = {};
-        this._interpState = {};
+        this._prevWorld = {};
+        this._interpWorld = {};
 };
 
 JSTest.GameStates.Init.prototype.draw = function()
@@ -30,7 +68,7 @@ JSTest.GameStates.Init.prototype.draw = function()
 	ctx.save();
 
 	ctx.translate(this._canvas.width/2, this._canvas.height/2);
-	ctx.rotate(Math.PI * this._interpState.angle);
+	ctx.rotate(Math.PI * this._interpWorld.angle);
 	ctx.fillText('关谷个傻逼！', 0, 0);
 
 	ctx.restore();
@@ -44,13 +82,15 @@ JSTest.GameStates.Init.prototype.tick = function(delta)
     {
         this._timeAccumulator -= this._timeStep;
 
-        this._prevState = this._currentState;
-        this._currentState.angle += 0.0001 * this._timeStep;
-        this._currentState.angle %= 2.0;
+        this._prevWorld = this._currentWorld;
+
+        this._currentWorld.angle += 0.0001 * this._timeStep;
+        this._currentWorld.angle %= 2 * Math.PI;
     }
 
     var alpha = this._timeAccumulator / this._timeStep;
-    this._interpState.angle = alpha * this._currentState.angle + (1 - alpha) * this._prevState.angle;
+
+    this._interpWorld = this._currentWorld.interpolate(this._prevWorld, alpha);
 };
 
 JSTest.GameStates.Init.prototype.input = function()
