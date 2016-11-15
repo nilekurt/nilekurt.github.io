77d0a56b96e61c28336b0980500595edd2dac51a
nilekurt <nilekurt@gmail.com>
2016-11-15 21:14:41 +0800

    Added some more I2C code. More difficult than expected when considering edge triggering

--- js/I2CMaster.js
+++ js/I2CMaster.js
@@ -1,32 +1,40 @@
 var I2C = I2C || {}
 
-I2C.Master = function (bus)
+I2C.Master = function (bus, rate)
 {
     this._bus = bus;
+    this._rate = rate;
     this._currentCommand = null;
     this._commandQueue = [];
-    bus.addMaster(this);
 };
 
-I2C.Master.prototype.send(address, data, doneCallback)
+I2C.Master.prototype.send = function(address, data, doneCallback)
 {
     this._commandQueue.push({'send', address, data, doneCallback});
-    this.startProcessing();
 };
 
-I2C.Master.prototype.request(address, doneCallback)
+I2C.Master.prototype.request = function(address, doneCallback)
 {
     this._commandQueue.push({'request', null, address, doneCallback});
-    this.startProcessing();
 };
 
-I2C.Master.prototype.onTrigger(bit)
+I2C.Master.prototype.onTrigger = function()
 {
 };
 
-I2C.Master.prototype.startProcessing()
+
+I2C.Master.prototype.clkHigh = function()
 {
-    if (this._currentCommand === null)
-    {
-    }
+};
+
+I2C.Master.prototype.clkLow = function()
+{
+};
+
+I2C.Master.prototype.signalHigh = function()
+{
+};
+
+I2C.Master.prototype.signalLow = function()
+{
 };
